cb64c479df1df59cee74d120d12582445435e9f6
Akshay Kolhe <95869029+akshay-0405@users.noreply.github.com>
2021-12-24 21:24:36 +0530

    This C File Returns Sum Of digits .
    Returns Sum Of digits Accepting a Digit From USER.

---
+++ Recursion.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int abc(int n);
+void main()
+{
+	int n,total;
+	printf("To Check the Sum of Digits,\n");
+	printf("Enter a number :");
+	scanf("%d",&n);
+	total=abc(n);
+	printf("Sum Of Digits of %d is %d",n,total);
+}
+int abc(int n)
+{
+	if (n==0)
+	{
+		return 0;
+	}
+	else
+	{
+		return n%10+abc(n/10);
+	}
+}
